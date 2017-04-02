625bce2c0c4bab46bf04765a74fa73d8f264fb50
Elena <pchelena911@gmail.com>
2017-04-02 20:09:03 +0300

    Сборка

--- gulpfile.js
+++ gulpfile.js
@@ -6,25 +6,93 @@ var postcss = require("gulp-postcss");
 var precss = require("precss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
-var short = require("postcss-short");
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var svgmin = require("gulp-svgmin");
+var svgstore = require("gulp-svgstore");
+var run = require("run-sequence");
+var del = require("del");
 
 gulp.task("style", function() {
   gulp.src("postcss/style.css")
     .pipe(plumber())
     .pipe(postcss([
       precss(),
-      short(),
       autoprefixer({browsers: [
         "last 2 versions"
-      ]})
+      ]}),
+        mqpacker({
+        sort: true
+      })
     ]))
-    .pipe(gulp.dest("css"))
-    .pipe(server.stream());
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream())
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"));
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("images", function() {
+  return gulp.src("build/img/**/*.{png,jpg,gif}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true})
+    ]))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("sprite", function() {
+  return gulp.src("build/img/icons/*.svg")
+    .pipe(svgmin())
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("build", function(fn) {
+  run(
+    "clean",
+    "copy",
+    "style",
+    "images",
+    "sprite",
+    fn
+  );
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "fonts/**/*.{woff,woff2}",
+    "img/**",
+    "js/**",
+    "*.html"
+  ], {
+    base: "."
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task('clean', function() {
+  return del('build');
+});
+
+gulp.task('html:copy', function() {
+  return gulp.src('*.html')
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('html:update', ['html:copy'], function(done) {
+  server.reload();
+  done();
+});
+
+gulp.task("serve", function() {
   server.init({
-    server: ".",
+    server: "build",
     notify: false,
     open: true,
     cors: true,
